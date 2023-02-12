a2d2f6a2fb9ed85717c956e62982b77c64b9a2c4
hanibat8 <hanibatool09@gmail.com>
2023-02-12 19:52:09 +0500

    more build errors fix

--- components/landingPage/Table.js
+++ components/landingPage/Table.js
@@ -14,7 +14,7 @@ const Table = () => {
     </tr>
     <tr>
       <td className="bg-[#f4f7ff] uppercase px-8 py-4">
-        <span classNameName='w-4 h-4 p-3 py-2 mr-2 rounded-full border border-spacing-1 border-violet-400'>1</span>
+        <span className='w-4 h-4 p-3 py-2 mr-2 rounded-full border border-spacing-1 border-violet-400'>1</span>
         <span>TIME TO FIND AN ENGINEER</span>
         </td>
       <td className="bg-[#E8EEFE] border-white text-center border px-8 py-4">0-3 Days</td>
@@ -23,7 +23,7 @@ const Table = () => {
     </tr>
     <tr>
       <td className="bg-[#f4f7ff] uppercase px-8 py-4">
-      <span classNameName='w-4 h-4 p-3 py-2 mr-2 rounded-full border border-spacing-1 border-violet-400'>2</span>
+      <span className='w-4 h-4 p-3 py-2 mr-2 rounded-full border border-spacing-1 border-violet-400'>2</span>
        <span>RECRUITING FEE</span>
         </td>
       <td className=" bg-[#E8EEFE] border-white border text-center px-8 py-4">$0</td>
@@ -32,28 +32,28 @@ const Table = () => {
     </tr>
     <tr>
       <td className="bg-[#f4f7ff] uppercase px-8 py-4">
-      <span classNameName='w-4 h-4 p-3 py-2 mr-2 rounded-full border border-spacing-1 border-violet-400'>3</span>
+      <span className='w-4 h-4 p-3 py-2 mr-2 rounded-full border border-spacing-1 border-violet-400'>3</span>
         <span>QUALITY GUARANTEE</span>
         </td>
       <td className="bg-[#c2e5df] border-white border px-8 py-4">
-        <div classNameName='flex justify-center'>
+        <div className='flex justify-center'>
             <ArrowIcon color='#7FF287'/>
         </div>
       </td>
       <td className="bg-[#edc9cf] border px-8 py-4 text-center">
-      <div classNameName='flex justify-center'>
+      <div className='flex justify-center'>
         <CrossArrow />
       </div>
       </td>
       <td className="bg-[#edc9cf] border px-8 py-4 text-center">
-      <div classNameName='flex justify-center'>
+      <div className='flex justify-center'>
         <CrossArrow />
       </div>
       </td>
     </tr>
     <tr>
       <td className="bg-[#f4f7ff] uppercase px-8 py-4">
-      <span classNameName='w-4 h-4 p-3 py-2 mr-2 rounded-full border border-spacing-1 border-violet-400'>4</span>
+      <span className='w-4 h-4 p-3 py-2 mr-2 rounded-full border border-spacing-1 border-violet-400'>4</span>
       <span>FAILURE RATE</span></td>
       <td className="bg-[#E8EEFE] border-white border px-8 py-4 text-center">Very Low</td>
       <td className="bg-[#e4f2fe] border px-8 py-4 text-center">Low</td>
@@ -61,27 +61,27 @@ const Table = () => {
     </tr>
     <tr>
       <td className="bg-[#f4f7ff] uppercase px-8 py-4">
-      <span classNameName='w-4 h-4 p-3 py-2 mr-2 rounded-full border border-spacing-1 border-violet-400'>5</span>
+      <span className='w-4 h-4 p-3 py-2 mr-2 rounded-full border border-spacing-1 border-violet-400'>5</span>
       <span>PRE-SCREENED CANDIDATES</span></td>
       <td className="bg-[#c2e5df] border-white border px-8 py-4">
-      <div classNameName='flex justify-center'>
+      <div className='flex justify-center'>
             <ArrowIcon color='#7FF287'/>
         </div>
       </td>
       <td className="bg-[#edc9cf] border px-8 py-4 text-center">
-      <div classNameName='flex justify-center'>
+      <div className='flex justify-center'>
         <CrossArrow />
       </div>
       </td>
       <td className="bg-[#edc9cf] border px-8 py-4 text-center">
-      <div classNameName='flex justify-center'>
+      <div className='flex justify-center'>
         <CrossArrow />
       </div>
       </td>
     </tr>
     <tr>
       <td className="bg-[#f4f7ff] uppercase px-8 py-4">
-      <span classNameName='w-4 h-4 p-3 py-2 mr-2 rounded-full border border-spacing-1 border-violet-400'>6</span>
+      <span className='w-4 h-4 p-3 py-2 mr-2 rounded-full border border-spacing-1 border-violet-400'>6</span>
         <span>TERMINATION COSTS</span></td>
       <td className="bg-[#E8EEFE] border-white border px-8 py-4 text-center">None</td>
       <td className="bg-[#e4f2fe] border px-8 py-4 text-center">Very high</td>
